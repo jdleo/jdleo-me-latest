6689c1a86146aba4f61f138a8b27b63bc484d97c
John Leonardo <j@jdleo.me>
2025-01-05 19:03:09 -0800

    idk

--- src/app/apps/privacy/page.tsx
+++ src/app/apps/privacy/page.tsx
@@ -103,7 +103,7 @@ export default function Privacy() {
         },
         {
             title: 'Device Memory',
-            // @ts-ignore
+            // @ts-expect-error deviceMemory is not in the navigator types
             value: 'deviceMemory' in navigator ? `${(navigator as any).deviceMemory}GB` : 'Not Available',
             icon: '💾',
         },
